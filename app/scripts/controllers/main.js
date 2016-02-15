5a347f18bd44c1e482115dd2991ff463a368c212
tasos14 <tasos14tasos@gmail.com>
2016-02-15 14:56:44 +0200

    queen possible moves

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -51,11 +51,22 @@
         var i,j;
         // reset the table when grid changes
         for(i=1;i<vm.currentGrid+1;i++){
-          row = vm.cols[i-1];
-          angular.element('#'+row+i).empty();
+          for(j=1;j<vm.currentGrid+1;j++){
+            angular.element('#'+i+j).empty();
+
+            if(angular.element('#'+i+j).hasClass("red")){
+              angular.element('#'+i+j).removeClass("red");
+            }
+          }
           vm.cols[i-1] = 0;
           angular.element('#Q'+i).removeClass("fade");
         }
+        // for(i=1;i<vm.currentGrid+1;i++){
+        //   row = vm.cols[i-1];
+        //   angular.element('#'+row+i).empty();
+        //   vm.cols[i-1] = 0;
+        //   angular.element('#Q'+i).removeClass("fade");
+        // }
 
       };
       $scope.$watch(
@@ -69,15 +80,17 @@
 
       vm.moveQueen = function(event){
         // get the clicked tile's id
+        // get the clicked tile's id
         var id   = event.target.id;
+        var row,col;
 
         if(id.length == 2){
-          var line = id.charAt(0);
-          var col  = Number(id.charAt(1));
+          row = Number(id.charAt(0));
+          col = Number(id.charAt(1));
         }
         else{
-          var line = id.charAt(1);
-          var col  = Number(id.charAt(2));
+          row = Number(id.charAt(1));
+          col = Number(id.charAt(2));
         }
 
         // when you click a tile for the first time on a column
@@ -85,22 +98,72 @@
           angular.element('#Q'+col).addClass("fade");
           angular.element('#'+id).append('<img src="images/queen.png" '+
                                     'class="queen-'+vm.currentGrid+'" id="Q'+id+'" />');
-          vm.cols[col-1] = line;
+          vm.cols[col-1] = row;
+          vm.drawRedBlocks(row,col);
         }
         // when you click the tile whith a queen on it
-        else if(vm.cols[col-1] == line) {
+        else if(vm.cols[col-1] == row) {
           angular.element('#'+vm.cols[col-1]+col).empty();
           angular.element('#Q'+col).removeClass('fade');
           vm.cols[col-1] = 0;
+          vm.removeRedBlocks(Number(row),col);
         }
         // when you have already clicked a tile on that column
         else {
           angular.element('#'+vm.cols[col-1]+col).empty();
           angular.element('#'+id).append('<img src="images/queen.png" '+
                                     'class="queen-'+vm.currentGrid+'" id="Q'+id+'" />');
-          vm.cols[col-1] = line;
+          vm.drawRedBlocks(Number(row),col);
+          vm.removeRedBlocks(vm.cols[col-1],col);
+          vm.cols[col-1] = row;
+        }
+      };
+
+      vm.drawRedBlocks = function(row,col){
+        var i,j;
+        var absDestance;
+
+        for (i = 1; i<vm.currentGrid+1; i++) {
+          absDestance = Math.abs(i-col);
+
+          // draw the rows and cols
+          angular.element('#'+row+i).addClass('red');
+          angular.element('#'+i+col).addClass('red');
+
+          for(j=1; j<vm.currentGrid+1; j++){
+            // draw the diagonal
+            if(j == row-absDestance || j == row+absDestance){
+              angular.element('#'+j+i).addClass('red');
+            }
+          }
+        }
+        angular.element('#'+row+col).removeClass('red');
+      };
+
+      vm.removeRedBlocks = function(row,col){
+        var i,j;
+        var absDestance;
+
+        // remove from rows and cols
+        for(i = 1; i < vm.currentGrid+1; i++){
+          absDestance = Math.abs(i-col);
+          if(vm.cols[i-1] == 0){
+            angular.element('#'+row+i).removeClass('red');
+            angular.element('#'+i+col).removeClass('red');
+          }
+          // remove the diagonal
+          for(j=1; j < vm.currentGrid+1; j++){
+            if((j == row-absDestance || j== row+absDestance) ){
+              angular.element('#'+j+i).removeClass('red');
+            }
+          }
+        }
+
+        for (var i = 0; i < vm.currentGrid; i++) {
+          if(vm.cols[i] != 0){
+            vm.drawRedBlocks(vm.cols[i],i+1);
+          }
         }
 
       };
-
    });
