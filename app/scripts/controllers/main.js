552c18fbec17ed0ecf0ed4de422b145961c49ada
tasos14 <tasos14tasos@gmail.com>
2016-02-28 22:40:30 +0200

    removed selectors from main.js, now the code looks more like angular

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,6 +21,7 @@
      // example: vm.cols[1] = 3 -> there is a queen
      // in the third row of the first column
      vm.cols = [0,0,0,0];
+     vm.rows = [0,0,0,0];
 
      vm.gridSizes = [{
        name: "4x4",
@@ -56,9 +57,11 @@
         vm.currentGrid = gridSize;
 
         vm.cols = new Array(vm.currentGrid);
+        vm.rows = new Array(vm.currentGrid);
 
-        for(i=0;i<vm.cols.length;i++){
+        for(i=0;i<vm.currentGrid;i++){
           vm.cols[i] = 0;
+          vm.rows[i] = 0;
         }
 
         vm.newGame();
@@ -71,41 +74,16 @@
         var i,j;
 
         // reset the table when grid changes
-        for(i=1;i<vm.currentGrid+1;i++){
-          for(j=1;j<vm.currentGrid+1;j++){
-            angular.element('#'+i+j).empty();
-
-            if(angular.element('#'+i+j).hasClass("red")){
-              angular.element('#'+i+j).removeClass("red");
-            }
-          }
-          vm.cols[i-1] = 0;
-          angular.element('#Q'+i).removeClass("fade");
+        for(i=0;i<vm.currentGrid;i++){
+          vm.cols[i] = 0;
+          vm.rows[i] = 0;
         }
 
         vm.moves = 0;
         localStorage.setItem('moves',vm.moves);
         localStorage.setItem('cols',vm.cols);
-
-        // for(i=1;i<vm.currentGrid+1;i++){
-        //   row = vm.cols[i-1];
-        //   angular.element('#'+row+i).empty();
-        //   vm.cols[i-1] = 0;
-        //   angular.element('#Q'+i).removeClass("fade");
-        // }
-
+        localStorage.setItem('rows',vm.rows);
       };
-
-      // watch vm.currentGrid
-      $scope.$watch(
-        function watchCurrentGrid(scope){
-          return vm.currentGrid;
-        },
-        function handleGridChange(newValue, oldValue){
-
-        }
-      );
-
 
       vm.moveQueen = function(event){
         // get the clicked tile's id
@@ -124,107 +102,104 @@
 
         // when you click a tile for the first time on a column
         if(vm.cols[col-1] == 0){
-          angular.element('#Q'+col).addClass("fade");
-          angular.element('#'+id).append('<img src="images/queen.png" '+
-                                    'class="queen-'+vm.currentGrid+'" id="Q'+id+'" />');
           vm.cols[col-1] = row;
-          vm.drawRedBlocks(row,col);
+          vm.rows[row-1] = col;
+          //vm.drawRedBlocks(row,col);
           vm.moves++;
           localStorage.setItem('moves',vm.moves);
           localStorage.setItem('cols',vm.cols);
+          localStorage.setItem('rows',vm.rows);
         }
         // when you click the tile whith a queen on it
         else if(vm.cols[col-1] == row) {
-          angular.element('#'+vm.cols[col-1]+col).empty();
-          angular.element('#Q'+col).removeClass('fade');
           vm.cols[col-1] = 0;
-          vm.removeRedBlocks(row,col);
+          vm.rows[row-1] = 0;
+          //vm.removeRedBlocks(row,col);
           vm.moves++;
           localStorage.setItem('moves',vm.moves);
           localStorage.setItem('cols',vm.cols);
+          localStorage.setItem('rows',vm.rows);
         }
         // when you have already clicked a tile on that column
         else {
-          angular.element('#'+vm.cols[col-1]+col).empty();
-          angular.element('#'+id).append('<img src="images/queen.png" '+
-                                    'class="queen-'+vm.currentGrid+'" id="Q'+id+'" />');
           var prev_row =vm.cols[col-1];
+          //var prev_col =vm.rows[row-1];
           vm.cols[col-1] = row;
-          vm.removeRedBlocks(prev_row,col);
+          vm.rows[row-1] = col;
+          //vm.removeRedBlocks(prev_row,col);
           vm.moves++;
           localStorage.setItem('moves',vm.moves);
           localStorage.setItem('cols',vm.cols);
+          localStorage.setItem('rows',vm.rows);
         }
       };
 
-      vm.drawRedBlocks = function(row,col){
-        var i,j;
-        var absDestance;
-
-        for (i = 1; i<vm.currentGrid+1; i++) {
-          absDestance = Math.abs(i-col);
-
-          // draw the rows and cols
-          angular.element('#'+row+i).addClass('red');
-          angular.element('#'+i+col).addClass('red');
-
-          for(j=1; j<vm.currentGrid+1; j++){
-            // draw the diagonal
-            if(j == row-absDestance || j == row+absDestance){
-              angular.element('#'+j+i).addClass('red');
-            }
-          }
-        }
-        angular.element('#'+row+col).removeClass('red');
-      };
-
-      vm.removeRedBlocks = function(row,col){
-        var i,j;
-        var absDestance;
-
-        // remove from rows and cols
-        for(i = 1; i < vm.currentGrid+1; i++){
-          absDestance = Math.abs(i-col);
-          if(vm.cols[i-1] == 0){
-            angular.element('#'+row+i).removeClass('red');
-            angular.element('#'+i+col).removeClass('red');
-          }
-          // remove the diagonal
-          for(j=1; j < vm.currentGrid+1; j++){
-            angular.element('#'+j+i).removeClass('red');
-
-          }
-        }
-
-
-        for (var i = 0; i < vm.currentGrid; i++) {
-          if(vm.cols[i] != 0){
-            vm.drawRedBlocks(vm.cols[i],i+1);
-          }
-        }
-      };
+      // vm.drawRedBlocks = function(row,col){
+      //   var i,j;
+      //   var absDestance;
+      //
+      //   for (i = 1; i<vm.currentGrid+1; i++) {
+      //     absDestance = Math.abs(i-col);
+      //
+      //     // draw the rows and cols
+      //     angular.element('#'+row+i).addClass('red');
+      //     angular.element('#'+i+col).addClass('red');
+      //
+      //     for(j=1; j<vm.currentGrid+1; j++){
+      //       // draw the diagonal
+      //       if(j == row-absDestance || j == row+absDestance){
+      //         angular.element('#'+j+i).addClass('red');
+      //       }
+      //     }
+      //   }
+      //   angular.element('#'+row+col).removeClass('red');
+      // };
+      //
+      // vm.removeRedBlocks = function(row,col){
+      //   var i,j;
+      //   var absDestance;
+      //
+      //   // remove from rows and cols
+      //   for(i = 1; i < vm.currentGrid+1; i++){
+      //     absDestance = Math.abs(i-col);
+      //     if(vm.cols[i-1] == 0){
+      //       angular.element('#'+row+i).removeClass('red');
+      //       angular.element('#'+i+col).removeClass('red');
+      //     }
+      //     // remove the diagonal
+      //     for(j=1; j < vm.currentGrid+1; j++){
+      //       angular.element('#'+j+i).removeClass('red');
+      //
+      //     }
+      //   }
+      //
+      //
+      //   for (var i = 0; i < vm.currentGrid; i++) {
+      //     if(vm.cols[i] != 0){
+      //       vm.drawRedBlocks(vm.cols[i],i+1);
+      //     }
+      //   }
+      // };
 
       // get localStorage grid and cols if exist.
       // update vm.currentGrid and vm.cols
       // draw the queens on the board
-      vm.onload = function(){
-        var grid;
-        var cols;
-        var moves;
+      vm.init = function(){
+        var grid = Number(localStorage.getItem('gridSize'));
+        var cols = localStorage.getItem('cols').split(",",grid);
+        var rows = localStorage.getItem('rows').split(",",grid);
+        var moves = Number(localStorage.getItem('moves'));
         var i,row,col,id;
 
         if(grid != null){
-          grid = Number(localStorage.getItem('gridSize'));
           vm.currentGrid = grid;
         }
 
         if(moves != null){
-          moves = Number(localStorage.getItem('moves'));
           vm.moves = moves;
         }
 
         if(cols != null){
-          cols = localStorage.getItem('cols').split(",",grid);
           vm.cols = new Array(grid);
           for(i=0;i<vm.cols.length;i++){
             vm.cols[i] = Number(cols[i]);
@@ -233,19 +208,23 @@
               row = vm.cols[i]
               col = i+1;
               id  = String(row)+String(col);
-
-              angular.element('#Q'+col).addClass("fade");
-              angular.element('#'+id).append('<img src="images/queen.png" '+
-                                        'class="queen-'+vm.currentGrid+'" id="Q'+id+'" />');
-              vm.drawRedBlocks(row,col);
+              //vm.drawRedBlocks(row,col);
             }
 
           }
         }
+
+        if (rows != null) {
+          vm.rows = new Array(grid);
+          for(i=0;i<vm.rows.length;i++){
+            vm.rows[i] = Number(rows[i]);
+            }
+        }
       };
 
-      setTimeout(vm.onload, 200);
-      angular.element('.container').ready(vm.onload);
+
+      vm.init();
+
 
 
    });
