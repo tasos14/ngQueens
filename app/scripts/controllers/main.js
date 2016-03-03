f6cee3d98bde8c836e0b90766aabbd4668fcc71b
tasos14 <tasos14tasos@gmail.com>
2016-03-03 13:04:36 +0200

    finished jquery to angular transition.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,14 +14,15 @@
 
      vm.currentGrid = 4;
 
-     vm.moves = 0;
+     vm.moves;
 
      // a table to track where the user clicked
      // each element represents a column
      // example: vm.cols[1] = 3 -> there is a queen
      // in the third row of the first column
-     vm.cols = [0,0,0,0];
-     vm.rows = [0,0,0,0];
+     vm.cols;
+     vm.rows;
+     vm.redBlocks;
 
      vm.gridSizes = [{
        name: "4x4",
@@ -56,14 +57,6 @@
         var i;
         vm.currentGrid = gridSize;
 
-        vm.cols = new Array(vm.currentGrid);
-        vm.rows = new Array(vm.currentGrid);
-
-        for(i=0;i<vm.currentGrid;i++){
-          vm.cols[i] = 0;
-          vm.rows[i] = 0;
-        }
-
         vm.newGame();
         localStorage.setItem('gridSize',gridSize);
       };
@@ -73,16 +66,33 @@
       vm.newGame = function(){
         var i,j;
 
+        vm.cols = new Array(vm.currentGrid);
+        vm.rows = new Array(vm.currentGrid);
+        vm.init_redBlocks(vm.currentGrid);
+
         // reset the table when grid changes
         for(i=0;i<vm.currentGrid;i++){
           vm.cols[i] = 0;
           vm.rows[i] = 0;
+          for(j=0;j<vm.currentGrid;j++){
+            vm.redBlocks[vm.currentGrid*i+j] = 0;
+          }
         }
 
         vm.moves = 0;
         localStorage.setItem('moves',vm.moves);
         localStorage.setItem('cols',vm.cols);
         localStorage.setItem('rows',vm.rows);
+      };
+
+      vm.init_redBlocks = function(gridSize){
+        var size = gridSize*gridSize;
+        var i;
+        vm.redBlocks = new Array(size);
+
+        for(i=0;i<size;i++){
+          vm.redBlocks[i] = 0;
+        }
       };
 
       vm.moveQueen = function(event){
@@ -104,8 +114,10 @@
         if(vm.cols[col-1] == 0){
           vm.cols[col-1] = row;
           vm.rows[row-1] = col;
-          //vm.drawRedBlocks(row,col);
           vm.moves++;
+
+          vm.drawRedBlocks(row,col);
+
           localStorage.setItem('moves',vm.moves);
           localStorage.setItem('cols',vm.cols);
           localStorage.setItem('rows',vm.rows);
@@ -114,7 +126,7 @@
         else if(vm.cols[col-1] == row) {
           vm.cols[col-1] = 0;
           vm.rows[row-1] = 0;
-          //vm.removeRedBlocks(row,col);
+          vm.removeRedBlocks(row,col);
           vm.moves++;
           localStorage.setItem('moves',vm.moves);
           localStorage.setItem('cols',vm.cols);
@@ -126,7 +138,7 @@
           //var prev_col =vm.rows[row-1];
           vm.cols[col-1] = row;
           vm.rows[row-1] = col;
-          //vm.removeRedBlocks(prev_row,col);
+          vm.removeRedBlocks(prev_row,col);
           vm.moves++;
           localStorage.setItem('moves',vm.moves);
           localStorage.setItem('cols',vm.cols);
@@ -134,97 +146,96 @@
         }
       };
 
-      // vm.drawRedBlocks = function(row,col){
-      //   var i,j;
-      //   var absDestance;
-      //
-      //   for (i = 1; i<vm.currentGrid+1; i++) {
-      //     absDestance = Math.abs(i-col);
-      //
-      //     // draw the rows and cols
-      //     angular.element('#'+row+i).addClass('red');
-      //     angular.element('#'+i+col).addClass('red');
-      //
-      //     for(j=1; j<vm.currentGrid+1; j++){
-      //       // draw the diagonal
-      //       if(j == row-absDestance || j == row+absDestance){
-      //         angular.element('#'+j+i).addClass('red');
-      //       }
-      //     }
-      //   }
-      //   angular.element('#'+row+col).removeClass('red');
-      // };
-      //
-      // vm.removeRedBlocks = function(row,col){
-      //   var i,j;
-      //   var absDestance;
-      //
-      //   // remove from rows and cols
-      //   for(i = 1; i < vm.currentGrid+1; i++){
-      //     absDestance = Math.abs(i-col);
-      //     if(vm.cols[i-1] == 0){
-      //       angular.element('#'+row+i).removeClass('red');
-      //       angular.element('#'+i+col).removeClass('red');
-      //     }
-      //     // remove the diagonal
-      //     for(j=1; j < vm.currentGrid+1; j++){
-      //       angular.element('#'+j+i).removeClass('red');
-      //
-      //     }
-      //   }
-      //
-      //
-      //   for (var i = 0; i < vm.currentGrid; i++) {
-      //     if(vm.cols[i] != 0){
-      //       vm.drawRedBlocks(vm.cols[i],i+1);
-      //     }
-      //   }
-      // };
+      vm.drawRedBlocks = function(row,col){
+        var i,j;
+        var absDist;
+
+        row--;
+        col--;
+        for(i=0;i<vm.currentGrid;i++){
+          absDist = Math.abs(i-col);
+
+          vm.redBlocks[vm.currentGrid*row+i] = 1;
+          vm.redBlocks[vm.currentGrid*i+col] = 1;
+          for(j=0;j<vm.currentGrid;j++){
+            if(j == row-absDist || j == row+absDist){
+              vm.redBlocks[vm.currentGrid*j+i] = 1;
+            }
+          }
+        }
+        vm.redBlocks[vm.currentGrid*row+col] = 0;
+      };
+
+      vm.removeRedBlocks = function(row,col){
+        var i,j;
+
+        row--;
+        col--;
+        for(i=0;i<vm.currentGrid;i++){
+
+          if(vm.cols[i] == 0){
+            vm.redBlocks[vm.currentGrid*row+i] = 0;
+            vm.redBlocks[vm.currentGrid*i+col] = 0;
+          }
+
+          for(j=0;j<vm.currentGrid;j++){
+            vm.redBlocks[vm.currentGrid*j+i] = 0;
+          }
+        }
+
+        for(i=0;i<vm.currentGrid;i++){
+          if(vm.cols[i] != 0){
+            vm.drawRedBlocks(vm.cols[i],i+1);
+          }
+        }
+      };
 
       // get localStorage grid and cols if exist.
       // update vm.currentGrid and vm.cols
       // draw the queens on the board
       vm.init = function(){
         var grid = Number(localStorage.getItem('gridSize'));
-        var cols = localStorage.getItem('cols').split(",",grid);
-        var rows = localStorage.getItem('rows').split(",",grid);
+        var cols = localStorage.getItem('cols') != null? localStorage.getItem('cols').split(",",grid) : null;
+        var rows = localStorage.getItem('rows') != null? localStorage.getItem('rows').split(",",grid) : null;
         var moves = Number(localStorage.getItem('moves'));
         var i,row,col,id;
 
-        if(grid != null){
+
+
+        if(grid != null && cols != null && rows != null && moves != null){
           vm.currentGrid = grid;
-        }
+          //vm.changeGridSize(grid);
+          vm.init_redBlocks(vm.currentGrid);
 
-        if(moves != null){
           vm.moves = moves;
-        }
 
-        if(cols != null){
           vm.cols = new Array(grid);
           for(i=0;i<vm.cols.length;i++){
             vm.cols[i] = Number(cols[i]);
+
             // if not 0 draw the queen
             if(vm.cols[i] != 0){
               row = vm.cols[i]
               col = i+1;
               id  = String(row)+String(col);
-              //vm.drawRedBlocks(row,col);
+              vm.drawRedBlocks(row,col);
             }
-
           }
-        }
 
-        if (rows != null) {
           vm.rows = new Array(grid);
           for(i=0;i<vm.rows.length;i++){
             vm.rows[i] = Number(rows[i]);
             }
+
         }
+        else {
+          vm.currentGrid = 4;
+          vm.newGame();
+
+        }
+
+
       };
 
-
       vm.init();
-
-
-
    });
